78b074986c68fa6dab22b4dbdddb6c0a3148859d
Phoenix Meadowlark <meadowlark@google.com>
2020-07-05 15:52:03 -0700

    Updates

---
+++ precache-manifest.489606a7994572f36b7f6efcc1a2416b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b0a6b1d608b916e8cdc07d30a6a961",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "660a219371c2f3ee44c4",
+    "url": "/resume/static/css/main.443899db.chunk.css"
+  },
+  {
+    "revision": "d193876f7df31131914a",
+    "url": "/resume/static/js/2.6e4b7816.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.6e4b7816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "660a219371c2f3ee44c4",
+    "url": "/resume/static/js/main.e4989fcc.chunk.js"
+  },
+  {
+    "revision": "2a0f0b5aee80e23eecf1",
+    "url": "/resume/static/js/runtime-main.dda0e86c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.5ede16c9206bf183cbfb3fd01c3729d8.js"
+  "/resume/precache-manifest.489606a7994572f36b7f6efcc1a2416b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
